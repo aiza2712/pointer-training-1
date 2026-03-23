1f924524658b1adda284f58c8dc16db97ef31b7e
aizahasya <eizzarihalatex@gmail.com>
2026-03-24 06:24:32 +0700

    just try

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+#include <memory>
 using namespace std;
 
 int main() {
@@ -20,5 +22,37 @@ int main() {
     for (int i = 0; i < 4; i++ ) {
         cout << "modif one values: " << *(arr + i) << endl;
     }
+
+    string tl;
+    cout << "masukan tanggal lahir (YYYY-MM-DD): ";
+    cin >> tl;
+
+    int year = stoi(tl.substr(0, 4));
+    int month = stoi(tl.substr(5, 2));
+    int day = stoi(tl.substr(8, 2));
+
+    unique_ptr<tm> birth = make_unique<tm>();
+    birth->tm_year = year - 1900;
+    birth->tm_mon = month - 1;
+    birth->tm_mday = day;
+
+    time_t now = time (0);
+    tm* current = localtime(&now);
+
+    int umur_tahun = (current->tm_year + 1900) - year;
+    int umur_bulan = ((current->tm_year + 1900) - year)*12;
+    int umur_hari = (current->tm_mday);
+
+
+
+    if((current->tm_mon + 1 == month) && (current->tm_mday < day)) {
+        umur_tahun--;
+    }
+
+    if((current->tm_mday < day)) {
+        umur_bulan--;
+    }
+
+    cout << "you are " << umur_tahun << "years old !! " << umur_bulan << " " << umur_hari << endl; 
 }
 
